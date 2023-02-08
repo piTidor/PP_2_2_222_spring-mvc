8869e616ed43420aeb15d5f315e7353918a82a87
piTidor <zerninandrei@gmail.com>
2023-02-08 23:41:41 +0300

    yes

--- src/main/java/web/controller/CarControler.java
+++ src/main/java/web/controller/CarControler.java
@@ -7,25 +7,39 @@ import org.springframework.web.bind.annotation.PathVariable;
 import web.model.Car;
 import web.service.CarService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarControler {
     @GetMapping(value = "/cars")
-    public String printWelcome(ModelMap model) {
-        List<Car> spisok = CarService.getCarForList(5);
-        model.addAttribute("cars", spisok);
-
-
-        return "cars";
-    }
-    @GetMapping("cars={count}")
-    public String printCar(ModelMap model,@PathVariable int count){
+    public String printWelcome(HttpServletRequest request, ModelMap model) {
+        int count ;
+        if (request.getParameter("count") == null){
+            count = 5;
+        } else {
+            count = Integer.parseInt(request.getParameter("count"));
+        }
         List<Car> spisok = CarService.getCarForList(count);
         model.addAttribute("cars", spisok);
 
 
         return "cars";
     }
+//    @GetMapping("={count}")
+//    public String printCar(ModelMap model,@PathVariable int count){
+//        List<Car> spisok = CarService.getCarForList(count);
+//        model.addAttribute("cars", spisok);
+//        return "cars";
+//    }
+//    	@GetMapping(value = "/")
+//	    public String Hello(ModelMap model) {
+//		List<String> messages = new ArrayList<>();
+//		messages.add("Hello!");
+//		messages.add("I'm Spring MVC application");
+//		messages.add("5.2.0 version by sep'19 ");
+//		model.addAttribute("messages", messages);
+//		return "index";
+//	}
 }
